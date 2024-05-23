4ba27069e73b7432e1070f681ae098a5fffbe9b0
oboard <oboard@outlook.com>
2024-05-23 11:59:51 +0800

    perf: edges

--- components/AvgCanvasView.tsx
+++ components/AvgCanvasView.tsx
@@ -2,55 +2,56 @@ import { useAppDispatch, useAppSelector } from "@/stores/hooks";
 import { nodeAdded, selectAvgCanvas, selectSelected } from "@/stores/store";
 import { v1 } from "uuid";
 import AvgNodeView from "./AvgNodeView";
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 export default function AvgCanvasView() {
   const data = useAppSelector(selectAvgCanvas);
   const selected = useAppSelector(selectSelected);
   const dispatch = useAppDispatch();
-  const [transform, setTransform] = useState({
-    x: 0,
-    y: 0,
-    scale: 1,
-  });
+  // const [transform, setTransform] = useState({
+  //   x: 0,
+  //   y: 0,
+  //   scale: 1,
+  // });
 
-  function getNodePos(nodeId: string, side: string) {
-    const node = data.nodes.find((n) => n.id === nodeId);
-    const posData = {
-      id: nodeId,
-      x: 0,
-      y: 0,
-    };
-    if (node) {
-      switch (side) {
-        case "top":
-          posData.x = node.x + node.width / 2;
-          posData.y = node.y;
-          break;
-        case "bottom":
-          posData.x = node.x + node.width / 2;
-          posData.y = node.y + node.height;
-          break;
-        case "left":
-          posData.x = node.x;
-          posData.y = node.y + node.height / 2;
-          break;
-        case "right":
-          posData.x = node.x + node.width;
-          posData.y = node.y + node.height / 2;
-          break;
-        default:
-          break;
+  const edgesData = useMemo(() => {
+    function getNodePos(nodeId: string, side: string) {
+      const node = data.nodes.find((n) => n.id === nodeId);
+      const posData = {
+        id: nodeId,
+        x: 0,
+        y: 0,
+      };
+      if (node) {
+        switch (side) {
+          case "top":
+            posData.x = node.x + node.width / 2;
+            posData.y = node.y;
+            break;
+          case "bottom":
+            posData.x = node.x + node.width / 2;
+            posData.y = node.y + node.height;
+            break;
+          case "left":
+            posData.x = node.x;
+            posData.y = node.y + node.height / 2;
+            break;
+          case "right":
+            posData.x = node.x + node.width;
+            posData.y = node.y + node.height / 2;
+            break;
+          default:
+            break;
+        }
       }
+      return posData;
     }
-    return posData;
-  }
-
-  const edgesData = data.edges.map((edge) => ({
-    source: getNodePos(edge.fromNode, edge.fromSide),
-    target: getNodePos(edge.toNode, edge.toSide),
-  }));
+    return data.edges.map((edge) => ({
+      source: getNodePos(edge.fromNode, edge.fromSide),
+      target: getNodePos(edge.toNode, edge.toSide),
+    }));
+  }, [data]);
 
   return (
     <div
